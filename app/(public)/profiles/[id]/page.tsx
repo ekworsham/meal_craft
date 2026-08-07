57b942baa42c655bc4c4cae8a4cccaf3cff62e4a
GutsySpider <mbowman@byupathway.edu>
2026-08-06 21:51:33 -0700

    update file structure added logo component removed my recipes button.

--- app/(public)/profiles/[id]/page.tsx
+++ app/(public)/profiles/[id]/page.tsx
@@ -1,7 +1,7 @@
-export default function MyRecipesPage() {
-  return (
-    <main>
-      <h1>My Place Holder</h1>
-    </main>
-  );
-}
+// export default function MyRecipesPage() {
+//   return (
+//     <main>
+//       <h1>My Place Holder</h1>
+//     </main>
+//   );
+// }
